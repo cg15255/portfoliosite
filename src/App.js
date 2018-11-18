f05a337b4492041615ff9c1460ef4d1d4455a99f
Chris George <chrisgeorge.9@gmail.com>
2018-11-17 21:24:16 -0800

    merging set state calls to one call instead of 4 separate calls for toggleMenuHandler

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     successClosed: {
       display: "block"
     },
-    loaded: true,
+    loaded: false,
     loadingStyles: {
       "display" : "flex",
       "opacity": "1"
@@ -41,6 +41,7 @@ class App extends Component {
       .then(response => {
         this.setState({
           wordpressData: response.data[0].acf,
+          loaded: true,
           loadingStyles: {"display": "none"},
           bodyStyles: {"opacity": "1", "transition": "0.6s"}
         });
@@ -54,56 +55,63 @@ class App extends Component {
 
   toggleMenuHandler = () => {
     if(!this.state.mobileMenuActive) {
-      this.setState({navMenuClass: "mobile-menu mobile-menu--open"});
-      this.setState({line1Class: "mobile-nav-button__line mobile-nav-button__line--1"});  
-      this.setState({line2Class: "mobile-nav-button__line mobile-nav-button__line--2"});  
-      this.setState({line3Class: "mobile-nav-button__line mobile-nav-button__line--3"});  
+      this.setState({
+        navMenuClass: "mobile-menu mobile-menu--open",
+        line1Class: "mobile-nav-button__line mobile-nav-button__line--1",
+        line2Class: "mobile-nav-button__line mobile-nav-button__line--2",
+        line3Class: "mobile-nav-button__line mobile-nav-button__line--3"
+      });
     } else {
-      this.setState({navMenuClass: "mobile-menu"});
-      this.setState({line1Class: "mobile-nav-button__line"});  
-      this.setState({line2Class: "mobile-nav-button__line"});  
-      this.setState({line3Class: "mobile-nav-button__line"});  
+      this.setState({
+        navMenuClass: "mobile-menu",
+        line1Class: "mobile-nav-button__line",
+        line2Class: "mobile-nav-button__line",
+        line3Class: "mobile-nav-button__line"
+      });
     }
     this.setState({mobileMenuActive: !this.state.mobileMenuActive});
   }
 
   render() {
+    let loadingDiv = (
+      <div className="loading" style={this.state.loadingStyles} ><div className="spinner spinner-1"></div></div>
+    );
+
+    if(this.state.loaded) {
+      loadingDiv = null;
+    }
+
     return (
-      <>
-        <div className="loading" style={this.state.loadingStyles} ><div className="spinner spinner-1"></div></div>
-        <HashRouter>
-          <div style={this.state.bodyStyles}>
-            <div className="App site-container">
-              <Header 
-                heroHeaderText={this.state.wordpressData.hero_header_text}
-                projectsPageHeader={this.state.wordpressData.projects_page_header} 
-                contactPageHeader={this.state.wordpressData.contact_page_header}
-                toggleMenuHandler={this.toggleMenuHandler}
-                navMenuClass={this.state.navMenuClass}
-                line1Class={this.state.line1Class}
-                line2Class={this.state.line2Class}
-                line3Class={this.state.line3Class} 
-              />
-              <Route path="/" exact render={() => <MainImage imageURL={this.state.wordpressData.main_image} />} />
-              <Body 
-              wordpressData={this.state.wordpressData} 
-              projects={this.state.projects} 
-              successClosed={this.state.successClosed}
-              closeHandler={this.closeHandler}
-              />
-              <Route path="/" exact render={() => <CallToAction 
-                callToActionText={this.state.wordpressData.call_to_action_text} 
-                buttonText={this.state.wordpressData.call_to_action_button_text} 
-              />} />
-              <Route path="/projects" exact render={() => <CallToAction 
-                callToActionText={this.state.wordpressData.call_to_action_text} 
-                buttonText={this.state.wordpressData.call_to_action_button_text} 
-              />} />      
-              <Footer footerText={this.state.wordpressData.footer_text} />
-            </div>
+      <HashRouter>
+          <div className="App site-container">
+            <Header 
+              heroHeaderText={this.state.wordpressData.hero_header_text}
+              projectsPageHeader={this.state.wordpressData.projects_page_header} 
+              contactPageHeader={this.state.wordpressData.contact_page_header}
+              toggleMenuHandler={this.toggleMenuHandler}
+              navMenuClass={this.state.navMenuClass}
+              line1Class={this.state.line1Class}
+              line2Class={this.state.line2Class}
+              line3Class={this.state.line3Class} 
+            />
+            <Route path="/" exact render={() => <MainImage imageURL={this.state.wordpressData.main_image} />} />
+            <Body 
+            wordpressData={this.state.wordpressData} 
+            projects={this.state.projects} 
+            successClosed={this.state.successClosed}
+            closeHandler={this.closeHandler}
+            />
+            <Route path="/" exact render={() => <CallToAction 
+              callToActionText={this.state.wordpressData.call_to_action_text} 
+              buttonText={this.state.wordpressData.call_to_action_button_text} 
+            />} />
+            <Route path="/projects" exact render={() => <CallToAction 
+              callToActionText={this.state.wordpressData.call_to_action_text} 
+              buttonText={this.state.wordpressData.call_to_action_button_text} 
+            />} />      
+            <Footer footerText={this.state.wordpressData.footer_text} />
           </div>
-        </HashRouter>
-      </>
+      </HashRouter>
     );
   }
 }
